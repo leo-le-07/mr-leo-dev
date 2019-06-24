0d114759b0dbf0341892e7ab4b559cdec7ea4a88
Leo <coder.leo.le@gmail.com>
2019-06-24 09:51:46 +0700

    Part 2 - Setup infrastructure for project

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,9 +1,41 @@
-/**
- * Configure your Gatsby site with this file.
- *
- * See: https://www.gatsbyjs.org/docs/gatsby-config/
- */
+const path = require('path')
 
 module.exports = {
-  /* Your site config here */
+  plugins: [
+    `gatsby-plugin-typescript`,
+    `gatsby-plugin-styled-components`,
+    {
+      resolve: 'gatsby-plugin-alias-imports',
+      options: {
+        alias: {
+          "@pages": path.resolve(__dirname, 'src/pages'),
+          "@components": path.resolve(__dirname, 'src/components'),
+          "@utils": path.resolve(__dirname, 'src/utils'),
+          "@constants": path.resolve(__dirname, 'src/constants'),
+          "@templates": path.resolve(__dirname, 'src/templates'),
+          "@static": path.resolve(__dirname, 'src/static'),
+        },
+        extensions: [],
+      },
+    },
+    {
+      resolve: `gatsby-plugin-typography`,
+      options: {
+        pathToConfigModule: `src/utils/typography.ts`,
+      },
+    },
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `Mr Leo`,
+        short_name: `mrleo`,
+        start_url: `/`,
+        background_color: `#222`,
+        theme_color: `#FFFFFF`,
+        display: `minimal-ui`,
+        icon: `static/gatsby-icon.png`,
+      },
+    },
+    `gatsby-plugin-offline`
+  ]
 }
