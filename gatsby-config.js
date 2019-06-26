5f93b6dd397ad8396ca4eefd2657db6a39e07cb7
Leo <coder.leo.le@gmail.com>
2019-06-26 11:42:12 +0700

    Configure format code styling markdown

--- gatsby-config.js
+++ gatsby-config.js
@@ -65,16 +65,16 @@ module.exports = {
     {
       resolve: `gatsby-transformer-remark`,
       options: {
-        // CommonMark mode (default: true)
-        commonmark: true,
-        // Footnotes mode (default: true)
-        footnotes: true,
-        // Pedantic mode (default: true)
-        pedantic: true,
-        // GitHub Flavored Markdown mode (default: true)
-        gfm: true,
-        // Plugins configs
-        plugins: [],
+        plugins: [
+          {
+            resolve: `gatsby-remark-images`,
+            options: {
+              maxWidth: 750,
+            },
+          },
+          `gatsby-remark-prismjs`,
+          `gatsby-remark-smartypants`,
+        ],
       },
     }
   ]
